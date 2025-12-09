34824141d267e0d33464eee42764844fa54b1253
KirillShatohin <kirillsatohin2356@gmail.com>
2025-12-10 01:26:00 +0500

    XAMZAT CHIMAEV

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,7 +2,8 @@
 
 def solve():
 # Ниже пишите решение задачи
-     
+    word1, word2 = input().split()
+    print(word1 == "awesome" or word2 == "awesome")
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
