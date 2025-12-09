c544ac353a81ef9624f504887dba2d39ab1a3bee
KirillShatohin <kirillsatohin2356@gmail.com>
2025-12-10 01:41:36 +0500

    sssss

--- tasks/task2.py
+++ tasks/task2.py
@@ -3,7 +3,7 @@
 def solve():
 # Ниже пишите решение задачи
     n = int(input())
-    print("True" if 5 <= n <= 19 else "False")
+    print(str(5 < n <= 19))
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
--- tasks/task3.py
+++ tasks/task3.py
@@ -2,8 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-    word1, word2 = input().split()
-    print("True" if word1 == "awesome" or word2 == "awesome" else "False")
+    parts = input().split()
+    word1 = parts[0]
+    word2 = parts[1] if len(parts) > 1 else ""
+    print(str(word1 == "awesome" or word2 == "awesome"))
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
