450487d884e6fec1c3926ede8f57434856e23849
KirillShatohin <kirillsatohin2356@gmail.com>
2025-12-10 21:35:25 +0500

    sss

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,11 +2,8 @@
 
 def solve():
 # Ниже пишите решение задачи
-    parts = input().split()
-    word1 = parts[0]
-    word2 = parts[1] if len(parts) > 1 else ""
+    word1, word2 = input().split()
     print(str(word1 == "awesome" or word2 == "awesome"))
-
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
